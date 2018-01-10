a5b9a590a5cb78aa8971aa2a259c35e73b511467
godlinpz <godlinpz@users.noreply.github.com>
2018-01-10 15:55:56 +0300

    Update youbotoved.user.js

--- youbotoved.user.js
+++ youbotoved.user.js
@@ -1,12 +1,13 @@
 // ==UserScript==
 // @name         YouBotoVed
 // @namespace    http://tampermonkey.net/
-// @version      0.11
+// @version      0.12
 // @description  try to take over the world!
 // @author       God
-// @match        https://*youtube.com/*
+// @match        https://youtube.com/*
 // @match        https://*.youtube.com/*
 // @grant        unsafeWindow
+// @require      https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js
 // ==/UserScript==
 
 unsafeWindow.$.getScript('https://github.com/godlinpz/youbotoved/raw/master/youbotoved.js');
