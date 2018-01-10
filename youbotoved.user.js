48ff1e430964c349e84f5cf685a7bd6c4271519f
godlinpz <godlinpz@users.noreply.github.com>
2018-01-10 16:08:39 +0300

    Update youbotoved.user.js

--- youbotoved.user.js
+++ youbotoved.user.js
@@ -8,6 +8,7 @@
 // @match        https://*.youtube.com/*
 // @grant        unsafeWindow
 // @require      https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js
+// @require      https://github.com/godlinpz/youbotoved/raw/master/youbotoved.js
 // ==/UserScript==
 
-unsafeWindow.$.getScript('https://github.com/godlinpz/youbotoved/raw/master/youbotoved.js');
+alert(1);
