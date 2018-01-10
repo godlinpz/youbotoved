02f70c104643f080abb18564b5f5367fcee035ef
godlinpz <godlinpz@users.noreply.github.com>
2018-01-10 15:43:18 +0300

    Create youbotoved.user.js

---
+++ youbotoved.user.js
@@ -0,0 +1,11 @@
+// ==UserScript==
+// @name         YouBotoVed
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  try to take over the world!
+// @author       God
+// @match        https://*youtube.com/*
+// @grant        unsafeWindow
+// ==/UserScript==
+
+unsafeWindow.$.getScript('https://github.com/...js');
